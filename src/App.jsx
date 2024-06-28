e4614e6555cd8520d34a49309e5e94fa55433810
HarithaMihimal <harithamihimal4@gmail.com>
2024-06-28 08:09:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css'
+import Header from './Components/Header'
+import Notes from './Components/Notes'
+
+function App() {
+
+
+  return (
+   <div className="main">
+      <Header />
+      <Notes />
+   </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='title'>My Notes</h1>
+      <div className="footer">
+        <p>&copy; 2024 Haritha Mihimal Wilwala Arachchi</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
